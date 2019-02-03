136a8a80687bbd2a2868a4ce9d9e7f9119f9a93a
jdbbaugh <jbaugh78@msn.com>
2019-02-03 14:12:59 -0600

    still working initial layout need to dig deeper into data management stuff

--- src/components/Gigster.js
+++ src/components/Gigster.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import ApplicationViews from "./ApplicationViews";
+import DataManager from '../modules/DataManager'
 import './Gigster.css';
 
 class Gigster extends Component {
   render() {
     return (
-      <div className="Gigster">
-        yo
-      </div>
+      <ApplicationViews />
     );
   }
 }
