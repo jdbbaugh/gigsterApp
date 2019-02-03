d45c9e9366094ef382e17562106a21c4ad5c153b
jdbbaugh <jbaugh78@msn.com>
2019-02-03 12:44:14 -0600

    inital layout

---
+++ src/components/Gigster.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Gigster.css';
+
+class Gigster extends Component {
+  render() {
+    return (
+      <div className="Gigster">
+        yo
+      </div>
+    );
+  }
+}
+
+export default Gigster;
