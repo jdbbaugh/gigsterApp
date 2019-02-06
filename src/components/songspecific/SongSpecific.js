0d355663280583fba8a3518bfa04e3a9345f86c2
jdbbaugh <jbaugh78@msn.com>
2019-02-06 15:28:53 -0600

    now upon selecting the wanted song the screen displays youtube vid and other references for that chosen song

--- src/components/songspecific/SongSpecific.js
+++ src/components/songspecific/SongSpecific.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button'
+import { Link } from "react-router-dom"
+import ReactPlayer from 'react-player'
 
 export default class SongSpecific extends Component {
   render() {
     const chosenSongForViewing = this.props.songs.find(song => song.id === this.props.selectedArtistForSongsList);
     return (
-      <div>{chosenSongForViewing.songName}</div>
+      <div>
+      <Link to="/home">
+      <Button variant="secondary" size="lg" block>Return to Artist Display</Button>
+      </Link>
+        <h2>{chosenSongForViewing.songName}</h2>
+        <p>{chosenSongForViewing.writer}<br></br>-{chosenSongForViewing.genre}</p>
+        <ReactPlayer url={chosenSongForViewing.url}
+            playing
+            controls />
+
+        </div>
     )
   }
 }
