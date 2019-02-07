2ea95c5f7d078f9d91ac5d07f6765d3480ec5610
jdbbaugh <jbaugh78@msn.com>
2019-02-07 11:30:20 -0600

    now if the chord progression category is blank a button to add one is available

--- src/components/songspecific/ChordsForSpecific.js
+++ src/components/songspecific/ChordsForSpecific.js
@@ -43,7 +43,7 @@ export default class ChordsForSpecific extends Component {
   render() {
     return (
       <section className="progressionForSong">
-        <h4>Chord Chart:<a className="edit-name" onClick={this.progressionEditing}>   editChart</a></h4>
+      {this.props.selectedArtistForSongsList.progression === "" ? <Button onClick={this.progressionEditing} variant="dark">Add Chords</Button>: <h4>Chord Chart:<a className="edit-name" onClick={this.progressionEditing}>   editChart</a></h4>}
         {!this.state.editProgression ? <h2>{this.props.selectedArtistForSongsList.progression}</h2> :
           <InputGroup>
             <InputGroup.Prepend>
